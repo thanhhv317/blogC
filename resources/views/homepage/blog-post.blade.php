3a2f6a08e65e6e04783ee46716b9e16f42152e3b
thanhhv317 <thanhhoang317@gmail.com>
2019-08-26 23:04:59 +0700

    resize content height blog

--- resources/views/homepage/blog-post.blade.php
+++ resources/views/homepage/blog-post.blade.php
@@ -26,17 +26,9 @@
 		<div class="row">
 			<!-- Post content -->
 			<div class="col-md-8">
-				<div class="section-row sticky-container">
+				<div class="section-row">
 					<div class="main-post">
 						{!! $post->content !!}
-					</div>
-					<div class="post-shares sticky-shares">
-						<a href="#/" class="share-facebook"><i class="fa fa-facebook"></i></a>
-						<a href="#/" class="share-twitter"><i class="fa fa-twitter"></i></a>
-						<a href="#/" class="share-google-plus"><i class="fa fa-google-plus"></i></a>
-						<a href="#/" class="share-pinterest"><i class="fa fa-pinterest"></i></a>
-						<a href="#/" class="share-linkedin"><i class="fa fa-linkedin"></i></a>
-						<a href="#/"><i class="fa fa-envelope"></i></a>
 					</div>
 				</div>
 
